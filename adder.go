8cf2f00bcae3f87a2d4147370ce4539081f7a7c3
Oleg Lipovchenko <oleg.lipovchenko@ask.com>
2026-01-14 23:20:40 -0500

    Add documentation

--- adder.go
+++ adder.go
@@ -1,5 +1,9 @@
+// Package adder provides a function to add two integers.
 package adder
 
+// Adder returns the sum of ints a and b.
+//
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
